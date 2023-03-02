02114b1d2c49ea179d3bedd52e8591282fdada3d
HetDave <dave.2@iitj.ac.in>
2023-03-02 23:51:33 +0530

    First commit

---
+++ app/src/main/java/com/example/travelbuddies/ToggleTravelActivity.kt
@@ -0,0 +1,8 @@
+package com.example.travelbuddies
+
+class ToggleTravelActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toggle_travel)
+    }
+}
